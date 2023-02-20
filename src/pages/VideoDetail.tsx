12f05497c874c48dead00412ec8144db5ef68762
seul-dev <seul.dev@gmail.com>
2023-02-20 16:00:50 +0900

    ✨ detail page 비디오 재생 기능

--- src/pages/VideoDetail.tsx
+++ src/pages/VideoDetail.tsx
@@ -1,7 +1,7 @@
 import type { Video } from '@projects/types/videos';
 import { useLocation } from 'react-router-dom';
 
-type RouteState = {
+type LocationState = {
   state: {
     video: Video;
   };
@@ -10,8 +10,18 @@ type RouteState = {
 export default function VideoDetail() {
   const {
     state: { video },
-  } = useLocation() as RouteState;
+  } = useLocation() as LocationState;
   const { title, channelId, channelTitle, description } = video.snippet;
 
-  return <div>{title}</div>;
+  return (
+    <div>
+      <iframe
+        id="player"
+        width="100%"
+        height="640"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        title={title}
+      />
+    </div>
+  );
 }
