e9c58ab60fbcc43953a5db00bdcba18e8c4e2344
seul-dev <seul.dev@gmail.com>
2023-02-22 19:49:20 +0900

    💄 update videolist, VideoCard styling

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -14,10 +14,15 @@ export default function VideoCard({ video }: Props) {
       onClick={() => {
         navigate(`videos/watch/${video.id}`, { state: { video } });
       }}
+      className="cursor-pointer"
     >
-      <img className="w-full" src={thumbnails.medium.url} alt={title} />
+      <img
+        className="w-full rounded-xl"
+        src={thumbnails.medium.url}
+        alt={title}
+      />
       <div>
-        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="font-semibold text-lg my-2 line-clamp-2">{title}</p>
         <p className="text-sm opacity-80">{channelTitle}</p>
         <p className="text-sm opacity-80">{formatAgo(publishedAt)}</p>
       </div>
